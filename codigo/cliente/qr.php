c171793303f293f11ebbfd49e43e45945654c46e
AlfredoCarias <55295824+AlfredoCarias@users.noreply.github.com>
2021-06-13 14:39:53 -0600

    Add files via upload

---
+++ codigo/cliente/qr.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <title>Document</title>
+</head>
+<body>
+<?php require_once("menu.php");?>
+<div class="amount-box text-center">
+
+	 <img src="../images/qr.jpg" alt="desc" width="345" height="450">
+     <h3><p class="amount">Acerque su telefono y utilice su escaner como se muestra en la imagen</p></h3>
+</div>
+</body>
+</html>
