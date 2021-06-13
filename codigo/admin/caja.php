0f70dd61f2afbf22249b838d8fceac939f4f297a
AlfredoCarias <55295824+AlfredoCarias@users.noreply.github.com>
2021-06-13 09:06:58 -0600

    Add files via upload

---
+++ codigo/admin/caja.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Corte de caja</title>
+    <link rel="stylesheet" href="contenedor.css">
+    <link rel="stylesheet" href="../cliente/wallet.css">
+    
+</head>
+<body>
+<?php require_once("menu.php");?>
+<div class="container" align="center" style="padding-top: 40px;">
+<label><input type="checkbox" id="cbox1" value="first_checkbox"> Este es mi primer checkbox</label><br>
+
+<input type="checkbox" id="cbox2" value="second_checkbox"> <label for="cbox2">Este es mi segundo checkbox</label>
+</div>
+</body>
+</html>
