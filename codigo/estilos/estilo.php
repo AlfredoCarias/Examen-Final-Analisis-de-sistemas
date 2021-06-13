cc03c3e23f9fd46355d0a19acb310bf410801887
AlfredoCarias <55295824+AlfredoCarias@users.noreply.github.com>
2021-06-13 09:05:09 -0600

    Add files via upload

---
+++ codigo/estilos/estilo.php
@@ -0,0 +1,53 @@
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <link rel="background" href="../css/style.css" rel="stylesheet" type="text/css">
+    <link rel="icon" type="image/png" sizes="16x16" href="../images/a.jpg">
+
+    <title>UMG PARK</title>
+
+    <link href="../css/lib/bootstrap/bootstrap.min.css" rel="stylesheet">
+
+    <link href="../css/lib/calendar2/semantic.ui.min.css" rel="stylesheet">
+    <link href="../css/lib/calendar2/pignose.calendar.min.css" rel="stylesheet">
+    <link href="../css/lib/owl.carousel.min.css" rel="stylesheet" />
+    <link href="../css/lib/owl.theme.default.min.css" rel="stylesheet" />
+    <link href="../css/helper.css" rel="stylesheet">
+    <link href="../css/style.css" rel="stylesheet">
+
+</head>
+
+    
+   
+    <script src="../js/lib/jquery/jquery.min.js"></script>
+    
+    <script src="../js/lib/bootstrap/js/popper.min.js"></script>
+    <script src="../js/lib/bootstrap/js/bootstrap.min.js"></script>
+  
+    <script src="../js/jquery.slimscroll.js"></script>
+ 
+    <script src="../js/sidebarmenu.js"></script>
+    <script src="../js/lib/sticky-kit-master/dist/sticky-kit.min.js"></script>
+
+<!--
+     <script src="../js/lib/morris-chart/raphael-min.js"></script>
+    <script src="../js/lib/morris-chart/morris.js"></script>
+    <script src="../js/lib/morris-chart/dashboard1-init.js"></script> -->
+
+
+    <script src="../js/lib/calendar-2/moment.latest.min.js"></script>
+    <script src="../js/lib/calendar-2/semantic.ui.min.js"></script>
+    <script src="../js/lib/calendar-2/prism.min.js"></script>
+    <script src="../js/lib/calendar-2/pignose.calendar.min.js"></script>
+    <script src="../js/lib/calendar-2/pignose.init.js"></script>
+
+    <script src="../js/lib/owl-carousel/owl.carousel.min.js"></script>
+    <script src="../js/lib/owl-carousel/owl.carousel-init.js"></script>
+    <script src="../js/scripts.js"></script>
+
+    <script src="../js/custom.min.js"></script>
