94461961b8413f08a24b306a8dc09754ffda9dd5
AlfredoCarias <55295824+AlfredoCarias@users.noreply.github.com>
2021-06-13 09:04:10 -0600

    Add files via upload

---
+++ codigo/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>UMG Park</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="../../vendors/iconfonts/mdi/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="../../vendors/css/vendor.bundle.base.css">
+  <link rel="stylesheet" href="../../vendors/css/vendor.bundle.addons.css">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="style.css">
+  <!-- endinject -->
+  <link rel="shortcut icon" href="images/a.jpg" />
+</head>
+
+<body style="background-image: url(images/bk.jpg);">
+  <div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper auth-page">
+      <div class="content-wrapper d-flex align-items-center auth auth-bg-1 theme-one">
+        <div class="row w-100">
+          <div class="col-lg-4 mx-auto">
+            <div class="auto-form-wrapper">
+              <form action="login/sesion.php" method="POST">
+                <div class="form-group">
+                  <label class="label">Usuario</label>
+                  <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Usuario" autofocus="usuario" name="usuario">
+                    <div class="input-group-append">
+                      <span class="input-group-text">
+                        <i class="mdi mdi-check-circle-outline"></i>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label class="label">Contaseña</label>
+                  <div class="input-group">
+                    <input type="password" class="form-control" placeholder="*********" name="pass">
+                    <div class="input-group-append">
+                      <span class="input-group-text">
+                        <i class="mdi mdi-check-circle-outline"></i>
+                      </span>
+                    </div>
+                  </div>
+                </div>
+                <div class="form-group">
+                  <button class="btn btn-primary submit-btn btn-block" name="sesion">Ingresar</button>
+                </div>
+                <div class="form-group d-flex justify-content-between">
+                  <div class="form-check form-check-flat mt-0">
+                  </div>
+                </div>
+              </form>
+            </div>
+            
+            <strong><p class="footer-text text-center text-danger">UMG Park</p></strong>
+          </div>
+        </div>
+      </div>
+      <!-- content-wrapper ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+
+</body>
+
+</html>
